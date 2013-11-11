b9dfde0410aef9305683487ea07ab64d8f7c1871
李洪祥 <gardeniaxy@gmail.com>
2013-11-12 01:19:57 +0800

    update pw

--- controllers/user.py
+++ controllers/user.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*- 
+
 import web
 import random
 from config import setting
@@ -19,4 +21,16 @@ class UpdatePw(User):
 		User.__init__(self)
 	def GET(self):
 		return render.user.update_pw(web.ctx.session, 'update_pw')
+	def POST(self):
+		i,role = web.input(), web.ctx.session.role
+		if i.new_password != i.new_password2:
+			return self.error('两次输入的新密码不匹配，请重新输入!')
+		if len(i.new_password) <= 3:
+			return self.error('新密码长度太短！<br>为了帐号安全，请设置比较复杂的密码。')
+		old = db.select(role, where=web.db.sqlwhere(\
+			{'id':web.ctx.session.uid, 'pw':i.old_password}))
+		if len(old) <= 0:
+			return self.error('原密码不正确，请重新输入!')
+		db.update(role, where=web.db.sqlwhere({'id':web.ctx.session.uid}), pw=i.new_password)
+		return self.success('密码修改成功！')
 		
