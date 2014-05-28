0028d17a20b2141547db7b528d7d4541b8c63a5c
郑力宁 <1730386018@qq.com>
2014-05-28 23:48:53 +0800

    Revert "set up done"
    This reverts commit 1a6a05f94513d9d80269b0d8e453e16ba7744bf7.

--- config/setting.py
+++ config/setting.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*- 
 import web
 
-render = web.template.render('/var/www/stu-select/templates/', cache=False)
-# db = web.database(dbn='sqlite', db='/var/www/stu-select/db/testdb')
+render = web.template.render('/var/www/mentor/stu-select/templates/', cache=False)
+db = web.database(dbn='sqlite', db='/var/www/mentor/stu-select/db/testdb')
 
-db = web.database(dbn='mysql', db='stu_select', user='root', pw='root')
+#db = web.database(dbn='mysql', db='select', user='root', pw='root')
 
 web.config.debug = True
 
@@ -12,15 +12,14 @@ config = web.storage(
 	site_name = U"本科生导师互选系统",
 	site_desc = '本科生导师互选系统',
 	email='gardeniaxy@gmail.com',
-	root = 'http://202.118.228.42/mentor',
-	refer = 'http://202.118.228.42/mentor/',
-	static = '/mentor/static',
-	app_name = 'mentor',
+	root = 'http://mentor.cs.hit.edu.cn:8080',
+	refer = 'http://mentor.cs.hit.edu.cn:8080/',
+	static = '/static',
 	# 年级
 	grade = '1',
 )
 
-default_photo_url = '/mentor/static/img/avatar.png'
+default_photo_url = '/static/img/avatar.png'
 
 web.template.Template.globals['config'] = config
 web.template.Template.globals['render'] = render
